82900a23f0a3beec8db43b6ab26762ecd48e0d37
Katie <katracey@gmail.com>
2015-11-17 14:28:56 -0500

    1

---
+++ 150914_Divisible/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _150914_Divisible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            string number = Console.ReadLine();
+            if (int.Parse(number) % 5 == 0 && int.Parse(number) % 7 == 0)
+            {
+                Console.WriteLine("Divisble by 5 and 7 with no remainder.");
+            }
+            else
+            {
+                Console.WriteLine("Not divisble by 5 and 7 with no remainder.");
+            }
+
+        }
+    }
+}
